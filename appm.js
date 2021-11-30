53e23dc0444f977e35071f4c1484146b221a65f6
Deep2989 <deepikajoge@gmail.com>
2021-11-30 22:54:11 +0530

    change portnumber

--- appm.js
+++ appm.js
@@ -7,7 +7,7 @@ dotenv.config();
 const mongoUrl='mongodb+srv://LocationDataTest:test@cluster0.sae9v.mongodb.net/Hotel?retryWrites=true&w=majority';
 var cors=require('cors')
 const bodyParser=require('body-parser')
-var port=process.env.PORT||8124;
+var port=8275;/*process.env.PORT||8124;*/
 
 var db;
 
