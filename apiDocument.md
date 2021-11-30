89ffe136b6d009f7b7ea551a61c1a8de166edf00
Deep2989 <deepikajoge@gmail.com>
2021-11-30 15:01:28 +0530

    modeapi

---
+++ apiDocument.md
@@ -0,0 +1,38 @@
+Page 1 --- Home Page
+>> List of City
+(this will return all the city)
+
+>>Restaurant with respect to city name(on the basis of city return restaurants)
+
+>>List of all meals
+(return all the mealType)
+
+Page 2 ---- Listing details
+>>>Find restaurant on the basis of mealType
+(this will return all the restaurant of particular meal)
+
+>>>Filters
+>>>>>Cuisine filter
+(search on the basis of mealType and cuisine)
+>>>>Cost filter
+(search on the basis of mealType and the cost)
+>>>sort filters
+(price high to low and low to high)
+>>>>>cuisine + cost
+(search on the basis of mealType and cuisine + cost)
+
+Page 3 ---Restaurant Details
+>>>Get the detail of restaurant on basis of id
+http://localhost:8141/restaurantdata/8
+>>>>Menu with respect to restaurant details
+(return all items of menu for perticular restaurant)
+
+Page 4 ---- Summary Page
+>>>> Menu with respect to ids
+(provides item on basis of id)
+>>Post the order
+(Insert order details in db)
+
+Page 5 ---- Order Page
+>>>>List all the order placed
+
---
+++ appm.js
@@ -0,0 +1,89 @@
+var express=require('express');
+var app=express();
+var dotenv=require('dotenv');
+var mongo=require('mongodb');
+var MongoClient=mongo.MongoClient;
+dotenv.config();
+const mongoUrl='mongodb+srv://LocationDataTest:test@cluster0.sae9v.mongodb.net/Hotel?retryWrites=true&w=majority';
+var cors=require('cors')
+const bodyParser=require('body-parser')
+var port=process.env.PORT||8124;
+
+var db;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//first default route
+app.get('/',(req,res)=>{
+    res.send("Hii from Express")
+})
+
+app.get('/location',(req,res)=>{
+  db.collection('location').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+    
+})
+
+app.get('/locations',(req,res)=>{
+    db.collection('locations').find().toArray((err,result)=>{
+      if(err) throw err;
+      res.send(result)
+    })
+      
+  })
+
+  app.get('/mealType',(req,res)=>{
+  db.collection('mealType').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+app.get('/restaurantMenu',(req,res)=>{
+  db.collection('restaurantMenu').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+app.get('/restaurantdata',(req,res)=>{
+  db.collection('restaurantdata').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+app.get('/restaurantdata/:id',(req,res)=>{
+  var id=parseInt(req.params.id);
+  db.collection('restaurantdata').find({"restaurant_id":id}).toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+    
+  })
+
+  
+app.get('/orders',(req,res)=>{
+  db.collection('orders').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+/*app.get('/',(req,res)=>{
+  db.collection('').find().toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})*/
+MongoClient.connect(mongoUrl,(err,client)=>{
+  if(err) console.log("Error while connectiong")
+  db=client.db('Hotel')
+    app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+    })
+})
