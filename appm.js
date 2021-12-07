62f98c015edf5ec42ca6a3ce842fc4ffd2ee7e86
Deep2989 <deepikajoge@gmail.com>
2021-12-07 17:16:02 +0530

    all queries done except order

--- appm.js
+++ appm.js
@@ -4,7 +4,7 @@ var dotenv=require('dotenv');
 var mongo=require('mongodb');
 var MongoClient=mongo.MongoClient;
 dotenv.config();
-const mongoUrl='mongodb+srv://LocationDataTest:test@cluster0.sae9v.mongodb.net/Hotel?retryWrites=true&w=majority';
+const mongoUrl=process.env.MongoLiveUrl;
 var cors=require('cors')
 const bodyParser=require('body-parser')
 var port=process.env.PORT||8124;
@@ -20,23 +20,14 @@ app.get('/',(req,res)=>{
     res.send("Hii from Express")
 })
 
-app.get('/location',(req,res)=>{
-  db.collection('location').find().toArray((err,result)=>{
-    if(err) throw err;
-    res.send(result)
-  })
-    
-})
-
 app.get('/locations',(req,res)=>{
     db.collection('locations').find().toArray((err,result)=>{
       if(err) throw err;
       res.send(result)
-    })
-      
-  })
+    })      
+})
 
-  app.get('/mealType',(req,res)=>{
+app.get('/mealType',(req,res)=>{
   db.collection('mealType').find().toArray((err,result)=>{
     if(err) throw err;
     res.send(result)
@@ -57,14 +48,21 @@ app.get('/restaurantdata',(req,res)=>{
   })
 })
 
-app.get('/restaurantdata/:id',(req,res)=>{
+/*app.get('/restaurantdata/:id',(req,res)=>{
   var id=parseInt(req.params.id);
-  db.collection('restaurantdata').find({"restaurant_id":id}).toArray((err,result)=>{
+  db.collection('restaurantdata').find({"state_id":id}).toArray((err,result)=>{
     if(err) throw err;
     res.send(result)
-  })
-    
-  })
+  })    
+})*/
+
+app.get('/restaurantdata/:id',(req,res)=>{
+    var id=parseInt(req.params.id);
+    db.collection('restaurantdata').find({"restaurant_id":id}).toArray((err,result)=>{
+      if(err) throw err;
+      res.send(result)
+    })      
+})
 
 /*app.get('/filter/:mealId',(req,res)=>{
   var id=parseInt(req.params.mealId);
@@ -74,19 +72,42 @@ app.get('/restaurantdata/:id',(req,res)=>{
   })
 })*/
 
+//query on the basis of mealId and cuisine
 app.get('/filter/:mealId',(req,res)=>{
   var id=parseInt(req.params.mealId);
+  var sort={cost:1}
   var query={"mealTypes.mealtype_id":id}
 
-  if(req.query.cuisine){
-    query={"mealTypes.mealtype_id":id,"cuisines.cuisine_id":Number(req.query.cuisine)}
+  if(req.query.sortKey){
+    var sortKey=req.query.sortKey
+    if(sortKey>1 || sortKey<-1 || sortKey==0){
+        sortKey=1
+    }
+    sort={cost:Number(sortKey)}
   }
-  db.collection('restaurantdata').find(query).toArray((err,result)=>{
+
+  if(req.query.lcost&&req.query.hcost){
+    var lcost=Number(req.query.lcost);
+    var hcost=Number(req.query.hcost);
+  }
+
+  if(req.query.cuisine && req.query.lcost && req.query.hcost){
+    query={$and:[{cost:{$gt:lcost,$lt:hcost}}],"cuisines.cuisine_id":Number(req.query.cuisine),"mealTypes.mealtype_id":id}
+  } else if(req.query.cuisine){
+      query={"mealTypes.mealtype_id":id,"cuisines.cuisine_id":Number(req.query.cuisine)}
+    //query={"mealTypes.mealtype_id":id,"cuisines.cuisine_id":{$in:[2,5]}}
+  }
+//query on basis of cost
+  else if(req.query.lcost&&req.query.hcost){ 
+    query={$and:[{cost:{$gt:lcost,$lt:hcost}}],"mealTypes.mealtype_id":id}
+  }
+
+  db.collection('restaurantdata').find(query).sort(sort).toArray((err,result)=>{
     if(err) throw err;
     res.send(result)
   })
 })
-  
+
 app.get('/orders',(req,res)=>{
   db.collection('orders').find().toArray((err,result)=>{
     if(err) throw err;
@@ -94,12 +115,37 @@ app.get('/orders',(req,res)=>{
   })
 })
 
-/*app.get('/',(req,res)=>{
-  db.collection('').find().toArray((err,result)=>{
+/*app.get('/restaurantdata',(req,res)=>{
+  var query={};
+  if(req.query.city){
+    query={state_id:Number(req.query.city)}
+  }
+  db.collection('restaurantdata').find(query).toArray((err,result)=>{
     if(err) throw err;
     res.send(result)
   })
 })*/
+
+
+/*app.get('/restaurants',(req,res)=>{
+  var query={};
+  if(req.query.city){
+    query={name:Number(req.query.city)}
+  }
+  db.collection('restaurants').find(query).toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})*/
+
+app.get('/restaurantMenu/:restid',(req,res)=>{
+  var restid=Number(req.params.restid);
+  db.collection('restaurantMenu').find({"restaurant_id":restid}).toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
 MongoClient.connect(mongoUrl,(err,client)=>{
   if(err) console.log("Error while connectiong")
   db=client.db('Hotel')
